b530dc7148d3566c6f2f1880e3831eca912bcb33
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-08-07 20:11:03 +0430

    destinations infos changed to obj spreads

--- src/Main/MainContent.jsx
+++ src/Main/MainContent.jsx
@@ -7,6 +7,20 @@ import "./MainContent.css"
 
 
 function Main () {
+    const allDestination = [
+        {id: 1, name : "Trickle Creek Ranch" , img : "public/imgs/images/image1.jpg"},
+        {id: 2, name : "Dragonfly Tiny Cabin" , img : "public/imgs/images/image2.jpg"},
+        {id: 3, name : "Tiny Cabin in the mountains" , img : "public/imgs/images/image3.jpg"},
+        {id: 4, name : "The Stuga" , img : "public/imgs/images/image4.jpg"},
+        {id: 5, name : "Luxury Tiny Beach Cabin" , img : "public/imgs/images/image5.jpg"},
+        {id: 6, name : "The Summit Cabin" , img : "public/imgs/images/image6.jpg"},
+        {id: 7, name : "Kindred Spirits Cabin" , img : "public/imgs/images/image7.jpg"},
+        {id: 8, name : "The Hermitage Cabin" , img : "public/imgs/images/image8.jpg"},
+        {id: 9, name : "The Tree House" , img : "public/imgs/images/image9.jpg"},
+        {id: 10, name : "Barrier island elevated tent" , img : "public/imgs/images/image10.jpg"},
+        {id: 11, name : "Red Lifeguard Stand" , img : "public/imgs/images/image11.jpg"},
+        {id: 12, name : "Ninovan on the Shore" , img : "public/imgs/images/image12.jpg"},
+    ]
     return (
         <main>
         <div className="container">
@@ -24,18 +38,18 @@ function Main () {
                             & Activities
                         </h2>
                 </div>
-                <Destination name="Trickle Creek Ranch" img="public/imgs/images/image1.jpg" />
-                <Destination name="Dragonfly Tiny Cabin" img="public/imgs/images/image2.jpg" />
-                <Destination name="Tiny Cabin in the mountains" img="public/imgs/images/image3.jpg" />
-                <Destination name="The Stuga" img="public/imgs/images/image4.jpg" />
-                <Destination name="Luxury Tiny Beach Cabin" img="public/imgs/images/image5.jpg" />
-                <Destination name="The Summit Cabin" img="public/imgs/images/image6.jpg" />
-                <Destination name="Kindred Spirits Cabin" img="public/imgs/images/image7.jpg" />
-                <Destination name="The Hermitage Cabin" img="public/imgs/images/image8.jpg" />
-                <Destination name="Red Lifeguard Stand" img="public/imgs/images/image9.jpg" />
-                <Destination name="The Tree House" img="public/imgs/images/image10.jpg" />
-                <Destination name="Barrier island elevated tent" img="public/imgs/images/image11.jpg" />
-                <Destination name="Ninovan on the Shore" img="public/imgs/images/image12.jpg" />
+                <Destination {...allDestination[0]} />
+                <Destination {...allDestination[1]} />
+                <Destination {...allDestination[2]} />
+                <Destination {...allDestination[3]} />
+                <Destination {...allDestination[4]}/>
+                <Destination {...allDestination[5]} />
+                <Destination {...allDestination[6]} />
+                <Destination {...allDestination[7]} />
+                <Destination {...allDestination[8]} />
+                <Destination {...allDestination[9]}/>
+                <Destination {...allDestination[10]} />
+                <Destination {...allDestination[11]} />
                 <a href="#" className="destinations__link btn">View all</a>
             </section>
         </div>
