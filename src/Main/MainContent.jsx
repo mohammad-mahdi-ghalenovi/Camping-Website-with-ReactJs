18729bf7f5d6ed05769420a446a6a8c093391b00
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-08-07 22:38:42 +0430

    tent banner added

--- src/Main/MainContent.jsx
+++ src/Main/MainContent.jsx
@@ -31,12 +31,14 @@ function Main () {
     return (
         <main>
         <div className="container">
+
             <section className="services">
                 <Service {...AllServices[0]} />
                 <Service {...AllServices[1]} />
                 <Service {...AllServices[2]} />
                 <Service {...AllServices[3]} />
             </section>
+
             <section className="destinations">
                 <div className="destination-title-container">
                         <img src="../../public/imgs/svgs/destination.svg"  className="destination-title-container__icon" alt="" />
@@ -59,6 +61,27 @@ function Main () {
                 <Destination {...allDestinations[11]} />
                 <a href="#" className="destinations__link btn">View all</a>
             </section>
+
+            <section className="banner">
+                <div className="banner__blub"><img src="public/imgs/svgs/shape3.svg" alt="" /></div>
+                <div className="banner-left-content">
+                    <h2 className="banner-left-content__title">Let's Stay in Touch</h2>
+                    <p className="banner-left-content__caption">Get travel planning ideas, helpful tips, and stories from our visitors <br /> delivered right to your inbox.</p>
+                    <div className="banner-left-content__input-container">
+                        <span className="banner-left-content__icon email-icon">
+                            <img src="public/imgs/svg-icons/email.svg" alt="" />
+                        </span>
+                        <input type="text" className="" placeholder="Email Address" />
+                        <span className="banner-left-content__icon send-icon">
+                            <img src="public/imgs/svg-icons/send.svg" alt="" />
+                        </span>
+                    </div>
+                    </div>
+                <div className="banner-right-content">
+                    <img src="public/imgs/images/tent.png" alt="A tent" className="banner-right-content__img" />
+                </div>
+            </section>
+
         </div>
         </main>
 
